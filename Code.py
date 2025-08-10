e15b1779574f078bc7f09352b36450405fb29bf1
Chan Young Jung <wjdcksdud667@gmail.com>
2025-08-10 20:19:23 +0100

    Added all the visualisation of the 3 sorting algorithms

--- Code.py
+++ Code.py
@@ -1,3 +1,5 @@
+# Dependencies
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,44 +21,105 @@ elif spd == 3:
 # Generate an array integers from 1 to n then shuffle them
 array = np.random.permutation(np.arange(1, n+1))
 
-# represents the x-axis aka. the index of each element
+# Create three copies of the array for each sorting algorithm
+arr1 = array.copy()
+arr2 = array.copy()
+arr3 = array.copy()
+
+# Represents the x-axis aka. the index of each element
 x = np.arange(0, n, 1)
 
-# prints the original array
+# Prints the original array for the user to see
 print(array)
 
-# Bubble Sort visualiser
+
+# Just qol stuff.
 length = len(array)
 
+# Plot initial Selection and Insertion Sort graph (unsorted)
+plt.subplot(1,3,2)
+plt.title("Selection Sort Visualisation")
+plt.bar(x, arr2, color='orange')
+plt.xlabel("Index of item in list")
+plt.ylabel("Value")
+
+plt.subplot(1,3,3)
+plt.title("Insertion Sort Visualisation")
+plt.bar(x, arr3, color='limegreen')
+plt.xlabel("Index of item in list")
+plt.ylabel("Value")
+plt.pause(0.001)  # Short pause to render
+
+# Bubble Sort visualiser
 for i in range(length):
     for j in range(length - i - 1):
+        plt.subplot(1,3,1)
         plt.title("Bubble Sort Visualisation")
-        plt.bar(x, array, color='grey')
+        plt.bar(x, arr1, color='dodgerblue')
         plt.xlabel("Index of item in list")
         plt.ylabel("Value")
         plt.pause(speed)
-        plt.clf()
+        plt.cla()
+        # Bubble sort algorithm
+        if arr1[j] > arr1[j + 1]:
+            arr1[j], arr1[j + 1] = arr1[j + 1], arr1[j]
 
-        #bubble sort algorithm
-        if array[j] > array[j + 1]:
-            array[j], array[j + 1] = array[j + 1], array[j]
-
-# Plot the final sorted array so the chart stays visible
+# Plot the final sorted Bubble Sort visualisation so the chart stays visible
+plt.subplot(1,3,1)
 plt.title("Bubble Sort Visualisation")
-plt.bar(x, array, color='grey')
+plt.bar(x, arr1, color='green')
+plt.xlabel("Index of item in list")
+plt.ylabel("Value")
+plt.pause(0.001)  # Short pause to render
+
+# Selection Sort Visualiser
+for i in range(length):
+    cur_min_idx = i
+    for j in range(i+1, length):
+        plt.subplot(1,3,2)
+        plt.title("Selection Sort Visualisation")
+        plt.bar(x, arr2, color='orange')
+        plt.xlabel("Index of item in list")
+        plt.ylabel("Value")
+        plt.pause(speed)
+        plt.cla()
+
+        # Selection sort algorithm
+        if arr2[j] < arr2[cur_min_idx]:
+            cur_min_idx = j
+        else:
+            cur_min_idx = cur_min_idx
+    arr2[i], arr2[cur_min_idx] = arr2[cur_min_idx], arr2[i]
+
+plt.subplot(1,3,2)
+plt.title("Selection Sort Visualisation")
+plt.bar(x, arr2, color='green')
+plt.xlabel("Index of item in list")
+plt.ylabel("Value")
+plt.pause(0.001)  # Short pause to render
+
+
+# Insertion Sort visualiser
+
+
+for i in range(length):
+    for j in range(i+1, length):
+        plt.subplot(1,3,3)
+        plt.title("Insertion Sort Visualisation")
+        plt.bar(x, arr3, color='limegreen')
+        plt.xlabel("Index of item in list")
+        plt.ylabel("Value")
+        plt.pause(speed)
+        plt.cla()
+        
+        # Insertion sort algorithm
+        while arr3[j] < arr3[j-1] and j > 0:
+            arr3[j], arr3[j-1] = arr3[j-1], arr3[j]
+            j -= 1
+            
+plt.subplot(1,3,3)
+plt.title("Insertion Sort Visualisation")
+plt.bar(x, arr3, color='green')
 plt.xlabel("Index of item in list")
 plt.ylabel("Value")
 plt.show()
-
-
-#PSUEDO CODE FOR SELECTION SORT
-
-# in array check the first element
-# set the first element as a variable minimum
-# check next element compare with minimum
-# repeat until end of array
-# set first array to temp
-# set minimum to first element
-# set temp to where minimum was
-# check next element and repeat.
-# iteration stops at last element
