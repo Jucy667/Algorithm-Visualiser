05df7354a2fcd476a49d200976f11c16dc2f0f94
Chan Young Jung <wjdcksdud667@gmail.com>
2025-08-11 04:42:41 +0100

    Changes some code and added a counter for the amount of comparisons and swaps

--- Code.py
+++ Code.py
@@ -3,6 +3,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Initialising the counter variables
+
+bubble_comparisons = 0
+bubble_swaps = 0
+selection_comparisons = 0
+selection_swaps = 0
+insertion_comparisons = 0
+insertion_swaps = 0
+
 # Get the number of elements to sort
 n = int(input("Enter the number of elements you want to sort: "))
 
@@ -38,23 +47,25 @@ length = len(array)
 
 # Plot initial Selection and Insertion Sort graph (unsorted)
 plt.subplot(1,3,2)
-plt.title("Selection Sort Visualisation")
+plt.title(f"Selection Sort Visualisation\nComparisons: {selection_comparisons}  Swaps: {selection_swaps}")
 plt.bar(x, arr2, color='orange')
 plt.xlabel("Index of item in list")
 plt.ylabel("Value")
 
 plt.subplot(1,3,3)
-plt.title("Insertion Sort Visualisation")
+plt.title(f"Insertion Sort Visualisation\nComparisons: {insertion_comparisons}  Swaps: {insertion_swaps}")
 plt.bar(x, arr3, color='limegreen')
 plt.xlabel("Index of item in list")
 plt.ylabel("Value")
 plt.pause(0.001)  # Short pause to render
 
 # Bubble Sort visualiser
+
 for i in range(length):
-    for j in range(length - i - 1):
+    for j in range(0,length - i - 1):
+        bubble_comparisons += 1  # Increment the counter for each comparison
         plt.subplot(1,3,1)
-        plt.title("Bubble Sort Visualisation")
+        plt.title(f"Bubble Sort Visualisation\nComparisons: {bubble_comparisons}  Swaps: {bubble_swaps}")
         plt.bar(x, arr1, color='dodgerblue')
         plt.xlabel("Index of item in list")
         plt.ylabel("Value")
@@ -63,21 +74,24 @@ for i in range(length):
         # Bubble sort algorithm
         if arr1[j] > arr1[j + 1]:
             arr1[j], arr1[j + 1] = arr1[j + 1], arr1[j]
+            bubble_swaps += 1
 
 # Plot the final sorted Bubble Sort visualisation so the chart stays visible
 plt.subplot(1,3,1)
-plt.title("Bubble Sort Visualisation")
+plt.title(f"Bubble Sort Visualisation\nComparisons: {bubble_comparisons}  Swaps: {bubble_swaps}")
 plt.bar(x, arr1, color='green')
 plt.xlabel("Index of item in list")
 plt.ylabel("Value")
 plt.pause(0.001)  # Short pause to render
 
 # Selection Sort Visualiser
+
 for i in range(length):
     cur_min_idx = i
     for j in range(i+1, length):
+        selection_comparisons += 1  # Increment the counter for each comparison
         plt.subplot(1,3,2)
-        plt.title("Selection Sort Visualisation")
+        plt.title(f"Selection Sort Visualisation\nComparisons: {selection_comparisons}  Swaps: {selection_swaps}")
         plt.bar(x, arr2, color='orange')
         plt.xlabel("Index of item in list")
         plt.ylabel("Value")
@@ -89,36 +103,41 @@ for i in range(length):
             cur_min_idx = j
         else:
             cur_min_idx = cur_min_idx
-    arr2[i], arr2[cur_min_idx] = arr2[cur_min_idx], arr2[i]
+    if cur_min_idx != i:
+        arr2[i], arr2[cur_min_idx] = arr2[cur_min_idx], arr2[i]
+        selection_swaps += 1
 
 plt.subplot(1,3,2)
-plt.title("Selection Sort Visualisation")
+plt.title(f"Selection Sort Visualisation\nComparisons: {selection_comparisons}  Swaps: {selection_swaps}")
 plt.bar(x, arr2, color='green')
 plt.xlabel("Index of item in list")
 plt.ylabel("Value")
 plt.pause(0.001)  # Short pause to render
 
-
 # Insertion Sort visualiser
 
-
-for i in range(length):
-    for j in range(i+1, length):
-        plt.subplot(1,3,3)
-        plt.title("Insertion Sort Visualisation")
-        plt.bar(x, arr3, color='limegreen')
-        plt.xlabel("Index of item in list")
-        plt.ylabel("Value")
-        plt.pause(speed)
-        plt.cla()
+for i in range(1, length):
+    j = i
+    plt.subplot(1,3,3)
+    plt.title(f"Insertion Sort Visualisation\nComparisons: {insertion_comparisons}  Swaps: {insertion_swaps}")
+    plt.bar(x, arr3, color='limegreen')
+    plt.xlabel("Index of item in list")
+    plt.ylabel("Value")
+    plt.pause(speed)
+    plt.cla()
+    while j > 0:
+        insertion_comparisons += 1  # Increment the counter for each comparison
         
-        # Insertion sort algorithm
-        while arr3[j] < arr3[j-1] and j > 0:
+        if arr3[j] < arr3[j-1]:
             arr3[j], arr3[j-1] = arr3[j-1], arr3[j]
             j -= 1
-            
+            insertion_swaps += 1
+        else:
+            break
+    
+
 plt.subplot(1,3,3)
-plt.title("Insertion Sort Visualisation")
+plt.title(f"Insertion Sort Visualisation\nComparisons: {insertion_comparisons}  Swaps: {insertion_swaps}")
 plt.bar(x, arr3, color='green')
 plt.xlabel("Index of item in list")
 plt.ylabel("Value")
